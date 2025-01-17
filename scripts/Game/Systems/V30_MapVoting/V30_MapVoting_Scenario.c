f1751d3119b4bd7e7efbe1a89adee761a1274661
Vladislav Sazonov <vladsazonov@hotmail.com>
2025-01-17 20:32:02 +0300

    Remove addon list manipulation in V30_MapVoting_Scenario (Move to V30_MapVoting_Choice in future)

--- scripts/Game/Systems/V30_MapVoting/V30_MapVoting_Scenario.c
+++ scripts/Game/Systems/V30_MapVoting/V30_MapVoting_Scenario.c
@@ -3,39 +3,13 @@ class V30_MapVoting_Scenario {
 	
 	protected string m_AddonsList;
 	
-	protected static bool m_IsInitialAddonsList = false;
-	
-	protected static string m_InitialAddonsList;
-	
-	void V30_MapVoting_Scenario(ResourceName resourceName, string addonsList = "$initial") {
+	void V30_MapVoting_Scenario(ResourceName resourceName, string addonsList = string.Empty) {
 		m_ResourceName = resourceName;
 		m_AddonsList = addonsList;
 	};
 	
 	void Play() {
-		auto addonsList = m_AddonsList;
-		addonsList.Replace("$initial", GetInitialAddonsList());
-		addonsList.Replace("$current", GetCurrentAddonsList());
-		ArmaReforgerScripted.Cast(GetGame()).PlayGameConfig(m_ResourceName, addonsList);
-	};
-	
-	protected static string GetInitialAddonsList() {
-		if (m_IsInitialAddonsList) return m_InitialAddonsList;
-		m_InitialAddonsList = GetCurrentAddonsList();
-		return m_InitialAddonsList;
-	};
-	
-	protected static string GetCurrentAddonsList() {
-		auto addons = new array<string>();
-		GameProject.GetLoadedAddons(addons);
-		
-		string addonsList;
-		foreach (int i, string addon : addons) {
-			if (i > 0) addonsList += ",";
-			addonsList += addon;
-		};
-		
-		return addonsList;
+		ArmaReforgerScripted.Cast(GetGame()).PlayGameConfig(m_ResourceName, m_AddonsList);
 	};
 	
 	ResourceName GetResourceName() {
