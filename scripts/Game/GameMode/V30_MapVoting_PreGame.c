b716c2fb938da888c1f23884965c04282bfd663e
Vladislav Sazonov <vladsazonov@hotmail.com>
2025-10-02 21:47:16 +0300

    Added PreGameGameModeStateComponent

---
+++ scripts/Game/GameMode/V30_MapVoting_PreGame.c
@@ -0,0 +1,10 @@
+class V30_MapVoting_PreGameGameModeStateComponentClass : SCR_PreGameGameModeStateComponentClass {};
+
+class V30_MapVoting_PreGameGameModeStateComponent : SCR_PreGameGameModeStateComponent {
+    [Attribute("1", uiwidget: "slider", params: "1 128")]
+    protected int m_PlayerCountToStart;
+
+    override bool CanAdvanceState(SCR_EGameModeState nextState) {
+        return super.CanAdvanceState(nextState) && GetGame().GetPlayerManager().GetPlayerCount() >= m_PlayerCountToStart;
+    };
+};
