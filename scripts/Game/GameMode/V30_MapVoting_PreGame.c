9918f078d7015857e7dd12b986759a34dac62929
Vladislav Sazonov <vladsazonov@hotmail.com>
2025-10-04 00:51:41 +0300

    Allow 0 player count in PreGame component

--- scripts/Game/GameMode/V30_MapVoting_PreGame.c
+++ scripts/Game/GameMode/V30_MapVoting_PreGame.c
@@ -1,7 +1,7 @@
 class V30_MapVoting_PreGameGameModeStateComponentClass : SCR_PreGameGameModeStateComponentClass {};
 
 class V30_MapVoting_PreGameGameModeStateComponent : SCR_PreGameGameModeStateComponent {
-    [Attribute("1", uiwidget: "slider", params: "1 128")]
+    [Attribute("1", uiwidget: "slider", params: "0 128")]
     protected int m_PlayerCountToStart;
 
     override bool CanAdvanceState(SCR_EGameModeState nextState) {
