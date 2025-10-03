0d0887cf783dd24feccb62106e2d56e4dee579e2
Vladislav Sazonov <vladsazonov@hotmail.com>
2025-10-04 00:45:06 +0300

    Divide voting component to post game and scenario versions.

---
+++ scripts/Game/Components/Voting/PostGame.c
@@ -0,0 +1,3 @@
+class V30_MapVoting_PostGameVotingComponentClass : V30_MapVoting_VotingComponentClass {};
+
+class V30_MapVoting_PostGameVotingComponent : V30_MapVoting_VotingComponent {};
---
+++ scripts/Game/Components/Voting/Scenario.c
@@ -0,0 +1,3 @@
+class V30_MapVoting_ScenarioVotingComponentClass : V30_MapVoting_VotingComponentClass {};
+
+class V30_MapVoting_ScenarioVotingComponent : V30_MapVoting_VotingComponent {};
