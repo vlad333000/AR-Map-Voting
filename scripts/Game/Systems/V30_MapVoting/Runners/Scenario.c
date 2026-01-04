d8923aa36887fac025ad0338158a0ed65ceda864
Vladislav Sazonov <vladsazonov@hotmail.com>
2026-01-05 01:16:39 +0300

    Added SaveGameManager work before

--- scripts/Game/Systems/V30_MapVoting/Runners/Scenario.c
+++ scripts/Game/Systems/V30_MapVoting/Runners/Scenario.c
@@ -4,6 +4,14 @@ class V30_MapVoting_Runner_RequestScenarioChangeTransition : V30_MapVoting_Runne
 		auto scenarioId = runDataScenario.GetScenarioId();
 		auto addonsList = runDataScenario.GetAddonsList();
 		auto systemConfig = MissionHeader.ReadMissionHeader(scenarioId).GetWorldSystemsConfig();
-		GameStateTransitions.RequestScenarioChangeTransition(scenarioId, systemConfig, addonsList);
+
+		auto saveGameManager = GetGame().GetSaveGameManager();
+		if (saveGameManager)
+			saveGameManager.StartPlaythrough(scenarioId, transition: false);
+
+		if (GameStateTransitions.RequestScenarioChangeTransition(scenarioId, systemConfig, addonsList))
+			GetGame().GetMenuManager().CloseAllMenus();
+		else
+			PrintFormat("Failed to start scenario %1 with systemConfig %2 and addonsList %3.", scenarioId, systemConfig, addonsList, level: LogLevel.ERROR);;
 	};
 };
