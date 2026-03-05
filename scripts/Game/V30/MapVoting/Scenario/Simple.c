eb59ceed229e132763e05054af81560fb9a80c5e
Vladislav Sazonov <vladsazonov@hotmail.com>
2026-03-05 15:38:39 +0300

    Add setters for missionHeader in V30_MapVoting_SimpleScenario

--- scripts/Game/V30/MapVoting/Scenario/Simple.c
+++ scripts/Game/V30/MapVoting/Scenario/Simple.c
@@ -1,7 +1,15 @@
 class V30_MapVoting_SimpleScenario : V30_MapVoting_Scenario {
     protected ResourceName missionHeader;
 
+    void V30_MapVoting_SimpleScenario(ResourceName missionHeader) {
+        this.missionHeader = missionHeader;
+    };
+
     override ResourceName GetMissionHeader() {
         return missionHeader;
+    };
+
+    void SetMissionHeader(ResourceName missionHeader) {
+        this.missionHeader = missionHeader;
     };
 };
